43fb9a8b6b9ae014f90d4774396c6a1b02571d47
dzukp <pashazolo@mail.ru>
2020-02-19 00:20:14 +0300

    Добавлен проект для служебного HMI

---
+++ hmi/Data_hmi/scripts/post.js
@@ -0,0 +1,23 @@
+function selectPost(number) {
+	var prefix = 'p' + number + '_'	
+
+	getGlobalVar('post_cmd').setBindSpeaker(getTag('main', prefix + 'cmd'))
+	getGlobalVar('post_status').setBindSpeaker(getTag('main', prefix + 'status'))
+	getGlobalVar('post_pressure').setBindSpeaker(getTag('main', prefix + 'pressure'))
+	getGlobalVar('post_number').setBindSpeaker(getTag('main', prefix + 'number'))
+
+	var mechs = ['foam', 'wax', 'shampoo', 'cold_water', 'hot_water', 'osmos', 'out_water', 'out_foam', 'intensive']
+	for (var i = 0; i < mechs.length; i++ ) {
+		var s = mechs[i]
+
+		getGlobalVar('post_v_' + s + '_cmd').setBindSpeaker(getTag('main', prefix + 'v_' + s + '_cmd'))
+		getGlobalVar('post_v_' + s + '_status').setBindSpeaker(getTag('main', prefix + 'v_' + s + '_status'))
+	}
+
+	getGlobalVar('post_pump_cmd').setBindSpeaker(getTag('main', prefix + 'pump_cmd'))
+	getGlobalVar('post_pump_status').setBindSpeaker(getTag('main', prefix + 'pump_status'))
+	getGlobalVar('post_pump_auto_freq').setBindSpeaker(getTag('main', prefix + 'pump_auto_freq'))
+	getGlobalVar('post_pump_man_freq').setBindSpeaker(getTag('main', prefix + 'pump_man_freq'))
+	getGlobalVar('post_pump_out_freq').setBindSpeaker(getTag('main', prefix + 'pump_out_freq'))
+	getGlobalVar('post_pump_alarm').setBindSpeaker(getTag('main', prefix + 'pump_alarm'))
+}
