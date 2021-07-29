aec3888af6e839767b55dc06efed295a7fc6dbc7
dzukp <pashazolo@mail.ru>
2021-07-29 23:16:01 +0300

    fix objects.py tag binds

--- objects.py
+++ objects.py
@@ -552,7 +552,7 @@ def get_object(post_quantity=8):
         obj['children']['valve_intensive']['mb_cells_idx'] = start_addr + 21
         # obj['children']['pump']['mb_cells_idx'] = start_addr + 23
 
-        module_number = (post_number // 4) + 1
+        module_number = ((post_number - 1) // 4) + 1
         obj['children']['valve_foam']['do_open'] = f'do_{module_number}_{(post_number - 1) % 4 * 6 + 1}'
         obj['children']['valve_wax']['do_open'] = f'do_{module_number}_{(post_number - 1) % 4 * 6 + 2}'
         obj['children']['valve_shampoo']['do_open'] = f'do_{module_number}_{(post_number - 1) % 4 * 6 + 3}'
