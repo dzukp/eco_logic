bfce00da8a117ce9248ba4118fa795467014fb2b
pasha <pashazolo@mail.ru>
2023-06-14 20:44:06 +0300

    V2.1 Orel. Innovance

--- tagsrv_settings.py
+++ tagsrv_settings.py
@@ -21,8 +21,9 @@ def gen_tagsrv_config(post_quantity=(6, 6)):
     fc_names = tuple([
         f'fc_1_{i}_' for i in range(1, int(post_quantity[0] + 1))] + [
         f'fc_2_{i}_' for i in range(1, int(post_quantity[1] + 1))] + [
-        f'fc_os_', 'fc_hoover_1_', 'fc_hoover_2_']
+        f'fc_os_', 'fc_hoover_1_']
     )
+    fc_innovance_names = tuple(['fc_hoover_2_'])
 
     # generate ai_1_1 - ai_2_8
     for pref in ai_names:
@@ -51,6 +52,17 @@ def gen_tagsrv_config(post_quantity=(6, 6)):
     for pref in fc_names:
         tags['in'].update(dict([(f'{pref}ai_{i}', InTag(0x1875 + i - 1)) for i in range(1, 5)]))
         tags['out'].update(dict([(f'{pref}ao_{i}', OutTag(0x1870 + i - 1)) for i in range(1, 3)]))
+    for pref in fc_innovance_names:
+        tags['in'].update({
+            f'{pref}ai_1': InTag(0x3000),
+            f'{pref}ai_2': InTag(0x1001),
+            f'{pref}ai_3': InTag(0x8000),
+
+        })
+        tags['out'].update({
+            f'{pref}ao_1': OutTag(0x2000),
+            f'{pref}ao_2': OutTag(0x1000)
+        })
 
     ai_1_1 = OwenAiMv210(tags=[tag for name, tag in tags['in'].items() if name.startswith('ai_1_1_')],
                          ip='192.168.200.20', timeout=0.03)
