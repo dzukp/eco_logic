d1fd72449c473366423968eb02e0c327dd73134f
Zolotarev P <pvzolotarev@1cbit.ru>
2025-02-05 17:06:41 +0300

    iself. ПЧ на одном порту

--- tagsrv_settings.py
+++ tagsrv_settings.py
@@ -83,7 +83,7 @@ def gen_tagsrv_config(post_quantity=8):
 
     fc_modules_1 = []
 
-    for i in range(post_quantity):
+    for i in range(1, (post_quantity + 1)):
         fc_modules_1.append(ModbusRTUModule(i, port_1, io_tags=[], max_answ_len=5,
                                           in_tags=[tag for name, tag in tags['in'].items() if
                                                    name.startswith(f'fc{i}_ai_')],
