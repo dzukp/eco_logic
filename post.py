534ef6f6bb4916e3e172b1206989ce2239be53e3
Zolotarev P <pvzolotarev@1cbit.ru>
2025-02-20 19:42:24 +0300

    Волград ПЧ2. По одному клапану на ф-цию

--- post.py
+++ post.py
@@ -67,12 +67,12 @@ class Post(IoObject, ModbusDataObject):
         config = {'pump_on_timeout': self.pump_on_timeout, 'valve_off_timeout': self.valve_off_timeout,
                   'hi_press_valve_off_timeout': self.hi_press_valve_off_timeout}
         valves = {
-            FuncNames.FOAM: [self.valve_foam, self.valve_hot_water, self.valve_osmos, self.valve_cold_water],
-            FuncNames.SHAMPOO: [self.valve_shampoo, self.valve_hot_water, self.valve_cold_water],
-            FuncNames.WAX: [self.valve_wax, self.valve_cold_water],
-            FuncNames.HOT_WATER: [self.valve_hot_water, self.valve_cold_water],
+            FuncNames.FOAM: [self.valve_foam, ],
+            FuncNames.SHAMPOO: [self.valve_shampoo, ],
+            FuncNames.WAX: [self.valve_wax, ],
+            FuncNames.HOT_WATER: [self.valve_hot_water, ],
             FuncNames.COLD_WATER: [self.valve_cold_water],
-            FuncNames.OSMOSIS: [self.valve_osmos, self.valve_cold_water],
+            FuncNames.OSMOSIS: [self.valve_osmos, ],
             FuncNames.INTENSIVE: [self.valve_intensive]
         }
         for func_name, step in self.func_steps.items():
